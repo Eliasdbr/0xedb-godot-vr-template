2cfb1dbe2afbe0417d7e9c0f298a0ac40e1dad5b
Elias Borda <elias@androbin.co>
2025-09-01 00:46:33 -0300

    Menu scene, Added Cyclops, added models

---
+++ scenes/levels/zone_1_logic.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _ready() -> void:
+	pass
+
+# Push button, do something
+func _on_push_button_button_released(button: Variant) -> void:
+	pass
+
+
+func _on_push_button_button_pressed(button: Variant) -> void:
+	$"../CPUParticles3D".restart()
